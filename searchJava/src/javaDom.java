099656b4d193781f62107fcceb277c0784fa5ff2
LeeJun <ljun1118@ks.ac.kr>
2021-08-19 17:23:58 +0900

    delete spring boot && add java dom api

---
+++ searchJava/src/javaDom.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
+
+public class javaDom {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("src/resources/index.html"));
+
+        Element root = document.getDocumentElement();
+
+    }
+}
