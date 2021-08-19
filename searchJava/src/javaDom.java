6250099473b52ba34ae6bd8103abdf79aa87033b
LeeJun <ljun1118@ks.ac.kr>
2021-08-19 18:02:00 +0900

    learning java dom

--- searchJava/src/javaDom.java
+++ searchJava/src/javaDom.java
@@ -6,11 +6,12 @@ import java.io.*;
 
 public class javaDom {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+//        https://www.ibm.com/docs/ko/rsas/7.5.0?topic=files-xml-file-associations-dtds-xml-schemas
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(new File("src/resources/index.html"));
+        Document document = builder.parse(new File("searchJava/src/resources/index.html"));
 
         Element root = document.getDocumentElement();
-
+        System.out.println(root.getNodeName());
     }
 }
