1d43259a68406eb8b648da16b405f2b6607f33a5
LeeJun <ljun1118@ks.ac.kr>
2021-08-23 16:13:33 +0900

    alert 추가

--- javaSearch/src/main/java/com/search/javasearch/controller/searchController.java
+++ javaSearch/src/main/java/com/search/javasearch/controller/searchController.java
@@ -1,29 +1,23 @@
 package com.search.javasearch.controller;
 
-import com.search.javasearch.model.Message;
 import com.search.javasearch.model.searchWord;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class searchController {
 
-//    @RequestMapping(value = "/", method = RequestMethod.POST)
-//    @RequestMapping(value="/", method = RequestMethod.GET)
-//    @RequestMapping(name="/", method = RequestMethod.GET)
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("msg","input your id");
         return "index";
     }
 
-//    @RequestMapping(value="/", method = RequestMethod.GET)
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    @RequestMapping(value="/java", method = RequestMethod.GET)
+    @RequestMapping(value = "/java", method = RequestMethod.POST)
     public String send(@RequestParam("searchId") String nodeId,
                        @RequestParam("searchNode") String nodeName,
                        @RequestParam("attName") String attributeName,
@@ -33,6 +27,17 @@ public class searchController {
                 "nodeId : " + nodeId + "!!!!!"
         );
 
+       /* searchWord word = new searchWord();
+        word.setSearchId(nodeId);
+        word.setSearchNode(nodeName);
+        word.setAttName(attributeName);
+        word.setAttValue(attributeValue);
+
+        System.out.println(word.getSearchId());
+        System.out.println(word.getSearchNode());
+        System.out.println(word.getAttName());
+        System.out.println(word.getAttValue());
+*/
         model.addAttribute("nodeId",nodeId);
         model.addAttribute("nodeName",nodeName);
         model.addAttribute("attributeName",attributeName);
