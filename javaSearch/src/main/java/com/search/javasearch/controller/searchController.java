2d6096f3d2ba3748beb1a22b3244074274ff9b6d
LeeJun <ljun1118@ks.ac.kr>
2021-08-23 15:37:37 +0900

    html 에서 값 전달

--- javaSearch/src/main/java/com/search/javasearch/controller/searchController.java
+++ javaSearch/src/main/java/com/search/javasearch/controller/searchController.java
@@ -3,18 +3,42 @@ package com.search.javasearch.controller;
 import com.search.javasearch.model.Message;
 import com.search.javasearch.model.searchWord;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class searchController {
 
-    @GetMapping("/")
-    ModelAndView search(searchWord search, ModelAndView modelAndView){
+//    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    @RequestMapping(value="/", method = RequestMethod.GET)
+//    @RequestMapping(name="/", method = RequestMethod.GET)
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("msg","input your id");
+        return "index";
+    }
 
-        modelAndView.addObject("data", new Message("총 : "+" 개", "/"));
-        modelAndView.setViewName("index");
+//    @RequestMapping(value="/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String send(@RequestParam("searchId") String nodeId,
+                       @RequestParam("searchNode") String nodeName,
+                       @RequestParam("attName") String attributeName,
+                       @RequestParam("attValue") String attributeValue,
+                       Model model){
+        model.addAttribute("msg",
+                "nodeId : " + nodeId + "!!!!!"
+        );
 
-        return modelAndView;
+        model.addAttribute("nodeId",nodeId);
+        model.addAttribute("nodeName",nodeName);
+        model.addAttribute("attributeName",attributeName);
+        model.addAttribute("attributeValue",attributeValue);
+
+        System.out.println(nodeId);
+        return "index";
     }
 }
