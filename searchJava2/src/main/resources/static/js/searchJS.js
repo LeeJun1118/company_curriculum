ae4d2544f7c6526166153a933b6d9311ac9e8d2e
LeeJun <ljun1118@ks.ac.kr>
2021-08-24 17:19:55 +0900

    코드 위치 변경

--- searchJava2/src/main/resources/static/js/searchJS.js
+++ searchJava2/src/main/resources/static/js/searchJS.js
@@ -45,53 +45,9 @@ function nodeSearch() {
         alert("id : " + searchID + "    노드명 : " + searchNode + "    총 : " + countNodeId + "개");
     }
 
-    // //===================속성으로 찾기===================
-    // if (attName != '') {
-    //     var count = 0;
-    //     let nodeElements = [];
-    //     for(var i = 0; i < myHTML.length; i++){
-    //         // nodeElements[i] =  [id , class, onclick, ....]
-    //         nodeElements[i] = document.querySelector(myHTML[i]).getAttributeNames();
-    //         for(var j = 0; j < nodeElements[i].length; j++){
-    //             // x = id, class, onclick
-    //             var x = nodeElements[i];
-
-    //             //x[j]= 'class'   attName = 'class'
-    //             if (x[j] == attName) {
-    //                 count = count + document.querySelector(myHTML[i]).getAttribute(attName).length;
-    //             }
-    //         }
-    //     }
-    //     alert(attName + " : " + count);
-    // }
-
-    // //===================속성값으로만 찾기===================
-    // if (attValue != '') {
-    //     var countValue = 0;
-    //     for(var i = 0; i < myHTML.length; i++){
-    //         var tagList = document.querySelectorAll(myHTML[i]);
-    //         // id class onclick .........
-    //         for(var j = 0; j < tagList.length; j++){
-    //             var className = tagList[j].getAttributeNames();
-    //                 for(var k = 0; k < className.length; k++){
-    //                     var confirm = tagList[j].getAttributeNode(className[k]).value.split(' ');
-    //                     for(var h = 0; h < confirm.length; h++){
-    //                         if (confirm[h] == attValue) {
-    //                             countValue++;
-    //                         }
-    //                     }
-
-    //                 }
-    //         }
-    //     }
-    //     alert(attValue + " : " + countValue);
-    // }
-
-
-
     //=================== 속성명 + 속성값으로 찾기 ===================
     // 속성명, 속성값 같이 입력하게
-    // 속성명 빠졌을 때 속성명을 입력해주세요 
+    // 속성명 빠졌을 때 속성명을 입력해주세요
     // 속성값 빠졌을 때도 동일
     if (attName != '' && attValue == ''  /*&& searchNode == '' && searchID == '' */) {
         alert("속성값을 입력해주세요")
@@ -175,19 +131,61 @@ function nodeSearch() {
         for (var i = 0; i < tagList.length; i++) {
             // div li 등등 list 중에서 각 list에서  class id type의 값이 null 이 아니면
             //if (tagList[i].getAttribute('id') != null) {
-                // id가 searchID 라면
-                if (tagList[i].getAttribute('id') == searchID) {
-                    // class 의 value 가 여러개일 수 있으니 나눔
-                    var confirm = tagList[i].getAttributeNode(attName).value.split(' ');
-                    for (var h = 0; h < confirm.length; h++) {
-                        //class의 값들 중 attValue 와 같은게 있으면
-                        if (confirm[h] == attValue) {
-                            countAllCondition++;
-                        }
+            // id가 searchID 라면
+            if (tagList[i].getAttribute('id') == searchID) {
+                // class 의 value 가 여러개일 수 있으니 나눔
+                var confirm = tagList[i].getAttributeNode(attName).value.split(' ');
+                for (var h = 0; h < confirm.length; h++) {
+                    //class의 값들 중 attValue 와 같은게 있으면
+                    if (confirm[h] == attValue) {
+                        countAllCondition++;
                     }
                 }
+            }
             //}
         }
         alert("id : " + searchID + "    노드명 : " + searchNode + "    속성명 : " + attName + "    속성값 : " + attValue + "    총 : " + countAllCondition + "개");
     }
+
+    // //===================속성으로 찾기===================
+    // if (attName != '') {
+    //     var count = 0;
+    //     let nodeElements = [];
+    //     for(var i = 0; i < myHTML.length; i++){
+    //         // nodeElements[i] =  [id , class, onclick, ....]
+    //         nodeElements[i] = document.querySelector(myHTML[i]).getAttributeNames();
+    //         for(var j = 0; j < nodeElements[i].length; j++){
+    //             // x = id, class, onclick
+    //             var x = nodeElements[i];
+
+    //             //x[j]= 'class'   attName = 'class'
+    //             if (x[j] == attName) {
+    //                 count = count + document.querySelector(myHTML[i]).getAttribute(attName).length;
+    //             }
+    //         }
+    //     }
+    //     alert(attName + " : " + count);
+    // }
+
+    // //===================속성값으로만 찾기===================
+    // if (attValue != '') {
+    //     var countValue = 0;
+    //     for(var i = 0; i < myHTML.length; i++){
+    //         var tagList = document.querySelectorAll(myHTML[i]);
+    //         // id class onclick .........
+    //         for(var j = 0; j < tagList.length; j++){
+    //             var className = tagList[j].getAttributeNames();
+    //                 for(var k = 0; k < className.length; k++){
+    //                     var confirm = tagList[j].getAttributeNode(className[k]).value.split(' ');
+    //                     for(var h = 0; h < confirm.length; h++){
+    //                         if (confirm[h] == attValue) {
+    //                             countValue++;
+    //                         }
+    //                     }
+
+    //                 }
+    //         }
+    //     }
+    //     alert(attValue + " : " + countValue);
+    // }
 }
