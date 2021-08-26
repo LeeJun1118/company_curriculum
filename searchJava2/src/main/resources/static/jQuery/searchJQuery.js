02518c9af57a573b6659b755e7524b4ee0a343ad
LeeJun <ljun1118@ks.ac.kr>
2021-08-26 13:53:03 +0900

    속성명 + 속성값으로 찾기 수정

--- searchJava2/src/main/resources/static/jQuery/searchJQuery.js
+++ searchJava2/src/main/resources/static/jQuery/searchJQuery.js
@@ -27,12 +27,28 @@ $(document).ready(function () {
         } else if (att_name == '' && att_value != '' ) {
             // alert("속성명을 입력해주세요")
             $("#inJQuery").val("속성명을 입력해주세요");
-        } else if (att_name != '' && att_value != '' && search_id == '' && node == '') {
+          } else if (att_name != '' && att_value != '' && search_id == '' && node == '') {
+            if (att_name == "class")
+            {
+                $("#inJQuery").val($('.'+ att_value).length);
+            }
+            else{
             $("#inJQuery").val($('['+att_name + '='+ att_value + ']').length);
+
+            }
         }
 
         //========================id + 속성명 + 속성값 으로 찾기========================
         if (search_id != '' && node == '' && att_name != '' && att_value != '') {
+            //특정 id 인 속성 값들을 분리시켜야함
+
+            var array = $('[id' + '='+ search_id + ']').eq(0);
+            /*array.attr('['+att_name + '='+ att_value + ']');
+            array.hasClass('['+att_name + '='+ att_value + ']');
+*/
+            console.log(array)
+            // console.log(array.hasClass('['+att_name + '='+ att_value + ']'))
+            // console.log($("input[class=aa]").length)
             $("#inJQuery").val($('['+att_name + '='+ att_value + ']').filter('[id' + '='+ search_id + ']').length);
         }
 
