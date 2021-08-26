3eb7ff372e00dd88d541b0bfd7f2f0c66bae9c3c
LeeJun <ljun1118@ks.ac.kr>
2021-08-26 10:22:59 +0900

    id로 찾기

---
+++ searchJava2/src/main/resources/static/jQuery/searchJQuery.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+
+    $("#jQuery").click(function () {
+        var search_id = $("#searchID").val();
+        var node = $("#searchNode").val();
+        var att_name = $("#attName").val();
+        var att_value = $("#attValue").val();
+
+        //===================id 로 찾기===================
+        if (search_id != '' && node == '' && att_name == '' && att_value == '') {
+            $("#inJQuery").val($('[id' + '='+ search_id + ']').length);
+        }
+    })
+});
