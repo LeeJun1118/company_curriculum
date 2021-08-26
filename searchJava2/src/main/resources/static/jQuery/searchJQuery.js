a92f0b682c287ce672c32726a34fec9fb0441d9e
LeeJun <ljun1118@ks.ac.kr>
2021-08-26 10:44:03 +0900

    id + 속성명 + 속성값 으로 찾기

--- searchJava2/src/main/resources/static/jQuery/searchJQuery.js
+++ searchJava2/src/main/resources/static/jQuery/searchJQuery.js
@@ -28,10 +28,13 @@ $(document).ready(function () {
             // alert("속성명을 입력해주세요")
             $("#inJQuery").val("속성명을 입력해주세요");
         } else if (att_name != '' && att_value != '' && search_id == '' && node == '') {
-            console.log($('['+att_name + '='+ att_value + ']'))
             $("#inJQuery").val($('['+att_name + '='+ att_value + ']').length);
         }
 
+        //========================id + 속성명 + 속성값 으로 찾기========================
+        if (search_id != '' && node == '' && att_name != '' && att_value != '') {
+            $("#inJQuery").val($('['+att_name + '='+ att_value + ']').filter('[id' + '='+ search_id + ']').length);
+        }
 
     })
 });
