48ed8fe14532b1c5de942d1ae043e2becc47c9b9
LeeJun <ljun1118@ks.ac.kr>
2021-08-27 13:53:06 +0900

    게시판 init

--- JavaEEBoard/src/main/java/com/example/javaeeboard/BoardList.java
+++ JavaEEBoard/src/main/java/com/example/javaeeboard/BoardList.java
@@ -4,16 +4,16 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "create_board", value = "/board/create")
+public class BoardList extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "게시글 작성";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html;charset=UTF-8");
 
         // Hello
         PrintWriter out = response.getWriter();
--- JavaEEBoard/src/main/webapp/index.jsp
+++ JavaEEBoard/src/main/webapp/index.jsp
@@ -1,13 +1,12 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>JSP - Hello World</title>
-</head>
-<body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
-</body>
+    <head>
+        <title>Java EE Board</title>
+    </head>
+    <body>
+        <h1><%= "게시판" %></h1>
+        <br/>
+        <a href="/board/create">글 쓰기</a>
+    </body>
 </html>
