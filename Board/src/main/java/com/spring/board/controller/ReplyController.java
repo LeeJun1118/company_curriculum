d417c64d3602d2cebb3ecfe84e3beab2ea544e09
LeeJun <ljun1118@ks.ac.kr>
2021-10-01 12:52:28 +0900

    댓글 수정

--- Board/src/main/java/com/spring/board/controller/ReplyController.java
+++ Board/src/main/java/com/spring/board/controller/ReplyController.java
@@ -7,7 +7,9 @@ import com.spring.board.repository.ReplyRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,7 +23,7 @@ public class ReplyController {
     private final BoardRepository boardRepository;
 
     @PostMapping("/reply/new/{id}")
-    public String createBoard(@PathVariable("id") Long id,
+    public String createReply(@PathVariable("id") Long id,
                               @Valid ReplyForm replyForm,
                               BindingResult result) {
         if (result.hasErrors()) {
@@ -34,5 +36,21 @@ public class ReplyController {
         reply.setContent(replyForm.getContent());
         replyRepository.save(reply);
         return "redirect:/board/" + board.getId();
+    }
+
+    @PostMapping("/reply/update/{id}")
+    public String updateReply(@PathVariable("id") Long id,
+                              @Valid ReplyForm replyForm,
+                              BindingResult result) {
+        if (result.hasErrors()) {
+            return "boards/showBoard";
+        }
+        Reply reply = replyRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid reply Id:" + id));
+
+        reply.setContent(replyForm.getContent());
+
+        replyRepository.save(reply);
+        return "redirect:/board/" +id;
     }
 }
